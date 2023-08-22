151db04dceb35178ec43cb9911a40d2b6bf41bce
Sergey Naumov <svcomplex@gmail.com>
2023-08-22 23:03:59 +0300

    fas, доработки

---
+++ conf.fas/teamwork_ofp/ajax.py
@@ -0,0 +1,58 @@
+def regnumber(form,v):
+  v['regnumber']
+  return_hash={}
+
+  if ('regnumber' in v):
+    regnumber=v['regnumber']
+    after_html=''
+    
+    if regnumber.isnumeric():
+      if len(regnumber)>12:
+        link=f'https://zakupki.gov.ru/epz/order/notice/zk44/view/common-info.html?regNumber={regnumber}'
+        
+        
+        
+      else:
+        link=f'https://zakupki.gov.ru/223/purchase/public/purchase/info/common-info.html?regNumber={regnumber}'
+      
+      after_html=f'<a href="{link}" target="_blank">{link}</a>'
+      return_hash={'after_html':after_html}
+        
+  return ['regnumber',return_hash]
+      
+
+  #print('ajax regnumber:',v)
+
+def product(form,v):
+  firm=v['firm']
+  product=v['product']
+  bgcolor=''
+  
+  jscode=''
+  if product in ('9','14','15'):
+    bgcolor='#fdffc4'
+    
+  
+  if product=='8':
+    bgcolor='#ddffc4'
+  
+  if product=='11':
+    bgcolor='#c3c6ff'
+
+  if bgcolor:
+    #jscode=f"document.querySelector('form .v-card').style['background-color']='{bgcolor}'"
+    
+    # подменяем заголовок
+    #jscode=f"""document.querySelector('h1').innerHTML=`
+    #    <div style="display: inline-block; width: 50px; height: 50px; background-color: {bgcolor}; border: 1px solid gray;"></div>
+    #    <div style="display: inline-block; vertical-align: top;">{firm}</div>`"""
+
+    jscode=f"document.querySelector('.v-application--wrap .v-application--wrap').style.background='{bgcolor}'"
+
+  
+  return ['product',{'jscode':jscode}]
+
+ajax={
+  'regnumber':regnumber,
+  'product':product
+}
---
+++ conf.fas/teamwork_ofp/events_for_fields.py
@@ -0,0 +1,79 @@
+from config import config
+from lib.core_crm import get_manager, get_owner
+
+
+def firm_code(form,field):
+  #print('FIRM CODE!')
+  user_id=form.user_id
+  html=''
+  if user_id:
+    html=f'Карточка ОП: <a href="{config["BaseUrl"]}edit_form/user/{user_id}" target="_blank">{config["BaseUrl"]}edit_form/user/{user_id}</a>'
+  else:
+    html = "<span style='color: red'>id не было введено на этапе создания карточки СР</span>"
+
+  field['after_html']=html
+  
+
+
+# Менеджер
+def manager_before_code(form,field):
+  
+  if form.manager['login'] in ('sed','admin','akulov'):
+    
+    field['read_only']=False
+
+  name=field['name']
+  if form.ov and form.ov[name]:
+    manager_email=''
+    to_out=[]
+    if form.ov[f"{name}_email"]:
+      #manager_email=f'<a href="mailto:{form.ov["manager_from_email"]}">{form.ov["manager_from_email"]}</a> ;'
+      to_out.append(f'<a href="mailto:{form.ov[name+"_email"]}">{form.ov[name+"_email"]}</a>')
+    if form.ov[name+'_phone']:
+      to_out.append("тел: "+form.ov[name+'_phone'])
+
+    if len(to_out):
+      field['after_html']=f'''<small> {';'.join(to_out)}</small>'''
+
+    # для поля "менеджер" выводим руководителя
+    if name=='manager_from':
+      manager_from=get_manager(
+        id=form.ov['manager_from'],
+        db=form.db
+      )
+      if manager_from:
+        owner_from=get_owner(
+          cur_manager=manager_from,
+          db=form.db
+        )
+        if owner_from:
+          to_out=[]
+          if owner_from['name']: to_out.append(owner_from['name'])
+          if owner_from['email']: to_out.append(f'<a href="mailto:{owner_from["email"]}">{owner_from["email"]}</a>')
+          if owner_from["phone"]: to_out.append(owner_from["phone"])
+          if len(to_out):
+            field['after_html']+=f'<div style="margin-bottom: 20px;"><small>Руководитель: {" ; ".join(to_out)}</small></div>'
+      
+
+
+
+def manager_to2_before_code(form,field):
+  if form.manager['login'] in ('naumova','sheglova','sed','akulov','zia','strogov','pan'):
+    field['read_only']=False
+
+  manager_before_code(form,field)
+events={
+  'firm':{
+    'before_code':firm_code
+  },
+  'manager_from':{
+    'before_code':manager_before_code
+  },
+  'manager_to':{
+    'before_code':manager_before_code
+  },
+  'manager_to2':{
+    'before_code':manager_to2_before_code
+  }
+
+}
