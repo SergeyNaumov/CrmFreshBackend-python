b270bb4a6a7e1cb2d501fde261df79f49cce728d
Sergey Naumov <svcomplex@gmail.com>
2023-08-28 16:35:44 +0300

    конфиг юр. лица

---
+++ conf.fas/ur_lico/__init__.py
@@ -0,0 +1,173 @@
+ur_lico_filedir='files/ur_lico'
+
+form={
+    'work_table':'ur_lico',
+    'work_table_id':'id',
+    #'work_table_foreign_key':'project_id',
+    #'work_table_foreign_key_value':4664,
+    'title':'Юридические лица',
+    'sort':True,
+    'tree_use':True,
+    'explain':False,
+    'header_field':'url',
+    'default_find_filter':'header',
+    'fields': [ 
+        {
+            'description':'Вкл',
+            'type':'checkbox',
+            'name':'enabled',
+            'filter_on':True
+        },
+        {
+            'description':'Название организации',
+            'type':'text',
+            'name':'pnam',
+            'filter_on':True
+        },
+        {
+            'description':'Счета с НДС',
+            'type':'checkbox',
+            'name':'with_nds',
+            #'filter_on':True
+        },
+        {
+            'description':'Юр. Адрес',
+            'type':'text',
+            'name':'paddress'
+        },
+        {
+            'description':'Адрес',
+            'type':'text',
+            'name':'paddress2'
+        },
+        {
+            'description':'Расчетный счет и банк',
+            'type':'text',
+            'name':'prschet'
+        },
+        {
+            'description':'Расчетный счет',
+            'type':'text',
+            'name':'plic'
+        },
+        {
+            'description':'Банк',
+            'type':'text',
+            'name':'pbank'
+        },
+        {
+            'description':'БИК',
+          'type':'text',
+            'name':'pbik'
+        },
+        {
+            'description':'к/с',
+            'type':'text',
+            'name':'pkorschet'
+        },
+        {
+            'description':'ИНН',
+            'type':'text',
+            'name':'pinn'
+        },
+        {
+            'description':'КПП',
+            'type':'text',
+            'name':'pkpp'
+        },
+        {
+            'description':'ОГРН',
+            'type':'text',
+            'name':'pogrn'
+        },
+        {
+            'description':'Подпись ген. директора',
+            'type':'file',
+            'filedir':ur_lico_filedir,
+            'name':'imgpfaxgd'
+        },
+        {
+            'description':'Имя ген. директора',
+            'type':'text',
+            'name':'pgendir'
+        },
+        {
+            'description':'Имя ген. директора в родительном падеже',
+            'type':'text',
+            'name':'pgendir_r'
+        },
+        {
+            'description':'Имя ген. директора в дательном падеже',
+            'type':'text',
+            'name':'pgendir_d'
+        },
+        {
+            'description':'Подпись глав. бух.',
+            'type':'file',
+            'filedir':ur_lico_filedir,
+            'name':'imgpfaxgb'
+        },
+        {
+            'description':'Имя глав. бух',
+            'type':'text',
+            'name':'pglavbuh'
+        },
+        {
+            'description':'Печать',
+            'type':'file',
+            'filedir':ur_lico_filedir,
+            'name':'imgppechat'
+        },      
+        {
+            'description':'Печать с подписью',
+            'type':'file',
+            'filedir':ur_lico_filedir,
+            'name':'imgppechat2'
+        },      
+        {
+            'description':'Печать и подпись с рамкой',
+            'type':'file',
+            'filedir':ur_lico_filedir,
+            'name':'imgppodpech'
+        },
+        {
+            'description':'Для нерезидентов',
+            'type':'checkbox',
+            'name':'for_non_residents'
+        },
+        {
+            'description':'Diadoc Mailbox',
+            'type':'text',
+            'name':'diadoc_mailbox'
+        },
+        {
+            'description':'Diadoc login',
+            'type':'text',
+            'name':'diadoc_login'
+        },
+        {
+            'description':'Diadoc pass',
+            'type':'text',
+            'name':'diadoc_pass'
+        },
+        {
+            'description':'Diadoc key',
+            'type':'text',
+            'name':'diadoc_key'
+        },
+        {
+            'description':'Diadoc департамент',
+            'type':'text',
+            'name':'diadoc_dep'
+        },
+        {
+            'description':'Текст ссылки для счета',
+            'type':'text',
+            'name':'text_link'
+        },
+  ]  
+    
+}
+      
+
+
